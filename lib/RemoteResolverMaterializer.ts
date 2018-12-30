e46d07905e48fec986bf006b086a35fb4ccd61b9
kanekotic <alvarojosepl@gmail.com>
2018-12-30 13:24:14 -0600

    lint fix

--- lib/RemoteResolverMaterializer.ts
+++ lib/RemoteResolverMaterializer.ts
@@ -6,8 +6,8 @@ import IResolver from 'tarant/dist/actor-system/resolver/resolver'
 
 export class RemoteResolverMaterializer implements IResolver, IMaterializer {
   private resolvedActors: Set<Actor> = new Set()
-  private config : any
-  
+  private config: any
+
   constructor(config: any) {
     this.config = config
     setInterval(() => this.updateFromBackend(), config.sync.delay)
@@ -32,13 +32,13 @@ export class RemoteResolverMaterializer implements IResolver, IMaterializer {
 
   public async resolveActorById(id: string): Promise<Actor> {
     const result = await axios.get(`${this.config.paths.pull}/${id}`)
-    const actor = new (this.config.ActorTypes[result.data.type])(id)
+    const actor = new this.config.ActorTypes[result.data.type](id)
     actor.updateFrom(result.data)
     this.resolvedActors.add(actor)
     return Promise.resolve(actor)
   }
 
-  private updateFromBackend(){
+  private updateFromBackend() {
     this.resolvedActors.forEach((actor: IActor) =>
       axios.get(`${this.config.paths.pull}/${actor.id}`).then(result => (actor as any).updateFrom(result.data)),
     )
--- test/RemoteResolverMaterializer.spec.ts
+++ test/RemoteResolverMaterializer.spec.ts
@@ -1,162 +1,158 @@
-jest.useFakeTimers();
+jest.useFakeTimers()
 
 const mockAxios = {
-    post: jest.fn(),
-    get: jest.fn()
+  post: jest.fn(),
+  get: jest.fn(),
 }
 jest.mock('axios', () => ({ __esModule: true, default: mockAxios }))
 
 import { RemoteResolverMaterializer } from '../lib/RemoteResolverMaterializer'
 import * as faker from 'faker'
-import { Actor, ActorMessage } from 'tarant';
+import { Actor, ActorMessage } from 'tarant'
 
 class FakeActor {
-    constructor(id: string) {
-        this.id = id  
-    }
-    readonly id: string
-    toJson(){}
-    updateFrom(){}
+  constructor(id: string) {
+    this.id = id
+  }
+  readonly id: string
+  toJson() {}
+  updateFrom() {}
 }
 
 describe('RemoteResolverMaterializer', () => {
-    beforeEach(() => {
-        mockAxios.post.mockReset()
-        mockAxios.get.mockReset()
-    }); 
+  beforeEach(() => {
+    mockAxios.post.mockReset()
+    mockAxios.get.mockReset()
+  })
 
   describe('as a Resolver', () => {
     it('should try retrieve actor from remote', async () => {
-        const id = faker.random.uuid(),
-          config = {
-            sync: {
-                active: true,
-                delay: faker.random.number(1000)
-            },
-              paths: {
-                  pull: faker.internet.url(), 
-                  push: faker.internet.url(), 
-              },
-              ActorTypes: { FakeActor }
-          },
-          expectToJson = {
-              data:{
-                  type: "FakeActor",
-                  random: faker.random.uuid() 
-              }
-            }
-        mockAxios.get.mockResolvedValue(expectToJson)
-
-          let local = new RemoteResolverMaterializer(config)
-          let result = await local.resolveActorById(id)
-          expect(mockAxios.get).toHaveBeenCalledWith(`${config.paths.pull}/${id}`)
-          expect(result).toBeDefined()
-      
-    });      
-  });
-
-  describe('live sync', () => {
-
-      it('should sync actors that are in both sides if it was added by resolver', async () => {
-        const id = faker.random.uuid(),
+      const id = faker.random.uuid(),
         config = {
           sync: {
-              active: true,
-              delay: faker.random.number(1000)
+            active: true,
+            delay: faker.random.number(1000),
           },
-            paths: {
-                pull: faker.internet.url(), 
-                push: faker.internet.url(), 
-            },
-            ActorTypes: { FakeActor }
+          paths: {
+            pull: faker.internet.url(),
+            push: faker.internet.url(),
+          },
+          ActorTypes: { FakeActor },
         },
         expectToJson = {
-            data:{
-                type: "FakeActor",
-                random: faker.random.uuid(),
-                id
-            }
-          }
-        mockAxios.get.mockResolvedValue(expectToJson)
-
-        let local = new RemoteResolverMaterializer(config)
-        
-        await local.resolveActorById(id)
-        
-        mockAxios.get.mockClear()
-        jest.advanceTimersByTime(config.sync.delay);
-        
-        expect(mockAxios.get).toHaveBeenCalledWith(`${config.paths.pull}/${id}`)
-        expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), config.sync.delay)
-
-    });
-
-    it('should sync actors that are in both sides if it was added by materializer', async () => {
-        const id = faker.random.uuid(),
-        config = {
-          sync: {
-              active: true,
-              delay: faker.random.number(1000)
+          data: {
+            type: 'FakeActor',
+            random: faker.random.uuid(),
           },
-            paths: {
-                pull: faker.internet.url(), 
-                push: faker.internet.url(), 
-            },
-            ActorTypes: { FakeActor }
-        },
-        expectToJson = {
-            data:{
-                type: "FakeActor",
-                random: faker.random.uuid(),
-                id
-            }
-          },
-          actor = jest.fn<Actor>(() => ({ 
-            id, 
-            toJson: () => Promise.resolve(expectToJson),
-            updateFrom: jest.fn()
-          }))(),
-        actorMessage = jest.fn<ActorMessage>()()
-        
+        }
       mockAxios.get.mockResolvedValue(expectToJson)
 
-        let local = new RemoteResolverMaterializer(config)
-        
-        await local.onAfterMessage(actor, actorMessage)
-        
-        mockAxios.get.mockClear()
+      let local = new RemoteResolverMaterializer(config)
+      let result = await local.resolveActorById(id)
+      expect(mockAxios.get).toHaveBeenCalledWith(`${config.paths.pull}/${id}`)
+      expect(result).toBeDefined()
+    })
+  })
 
-        jest.advanceTimersByTime(config.sync.delay+10);
+  describe('live sync', () => {
+    it('should sync actors that are in both sides if it was added by resolver', async () => {
+      const id = faker.random.uuid(),
+        config = {
+          sync: {
+            active: true,
+            delay: faker.random.number(1000),
+          },
+          paths: {
+            pull: faker.internet.url(),
+            push: faker.internet.url(),
+          },
+          ActorTypes: { FakeActor },
+        },
+        expectToJson = {
+          data: {
+            type: 'FakeActor',
+            random: faker.random.uuid(),
+            id,
+          },
+        }
+      mockAxios.get.mockResolvedValue(expectToJson)
 
-        expect(mockAxios.get).toHaveBeenCalledWith(`${config.paths.pull}/${id}`)
-        expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), config.sync.delay)
+      let local = new RemoteResolverMaterializer(config)
 
-    });
-  });
+      await local.resolveActorById(id)
+
+      mockAxios.get.mockClear()
+      jest.advanceTimersByTime(config.sync.delay)
+
+      expect(mockAxios.get).toHaveBeenCalledWith(`${config.paths.pull}/${id}`)
+      expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), config.sync.delay)
+    })
+
+    it('should sync actors that are in both sides if it was added by materializer', async () => {
+      const id = faker.random.uuid(),
+        config = {
+          sync: {
+            active: true,
+            delay: faker.random.number(1000),
+          },
+          paths: {
+            pull: faker.internet.url(),
+            push: faker.internet.url(),
+          },
+          ActorTypes: { FakeActor },
+        },
+        expectToJson = {
+          data: {
+            type: 'FakeActor',
+            random: faker.random.uuid(),
+            id,
+          },
+        },
+        actor = jest.fn<Actor>(() => ({
+          id,
+          toJson: () => Promise.resolve(expectToJson),
+          updateFrom: jest.fn(),
+        }))(),
+        actorMessage = jest.fn<ActorMessage>()()
+
+      mockAxios.get.mockResolvedValue(expectToJson)
+
+      let local = new RemoteResolverMaterializer(config)
+
+      await local.onAfterMessage(actor, actorMessage)
+
+      mockAxios.get.mockClear()
+
+      jest.advanceTimersByTime(config.sync.delay + 10)
+
+      expect(mockAxios.get).toHaveBeenCalledWith(`${config.paths.pull}/${id}`)
+      expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), config.sync.delay)
+    })
+  })
 
   describe('as a Materializer', () => {
-      it('should send message if actor is updated', async () => {
-          const id = faker.random.uuid(),
-          config = {
-            sync: {
-                active: true,
-                delay: faker.random.number(1000)
-            },
-              paths: {
-                  pull: faker.internet.url(), 
-                  push: faker.internet.url(), 
-              }
+    it('should send message if actor is updated', async () => {
+      const id = faker.random.uuid(),
+        config = {
+          sync: {
+            active: true,
+            delay: faker.random.number(1000),
           },
-          expectToJson = {random: faker.random.uuid() },
-          actor = jest.fn<Actor>(() => ({ 
-              id, 
-              toJson: () => Promise.resolve(expectToJson),
-              updateFrom: jest.fn()
-            }))(),
-          actorMessage = jest.fn<ActorMessage>()()
-          let local = new RemoteResolverMaterializer(config)
-          await local.onAfterMessage(actor, actorMessage)
-          expect(mockAxios.post).toHaveBeenCalledWith(`${config.paths.push}/${id}`, expectToJson)
-      });
+          paths: {
+            pull: faker.internet.url(),
+            push: faker.internet.url(),
+          },
+        },
+        expectToJson = { random: faker.random.uuid() },
+        actor = jest.fn<Actor>(() => ({
+          id,
+          toJson: () => Promise.resolve(expectToJson),
+          updateFrom: jest.fn(),
+        }))(),
+        actorMessage = jest.fn<ActorMessage>()()
+      let local = new RemoteResolverMaterializer(config)
+      await local.onAfterMessage(actor, actorMessage)
+      expect(mockAxios.post).toHaveBeenCalledWith(`${config.paths.push}/${id}`, expectToJson)
+    })
   })
 })
